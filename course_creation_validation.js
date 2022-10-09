576beb6d9e9491740ad09a8a960f7c5d5087797d
trippieredd514 <annikatimermanis@gmail.com>
2022-10-09 14:39:30 -0400

    Update course_creation_validation.js

--- course_creation_validation.js
+++ course_creation_validation.js
@@ -64,9 +64,15 @@ function validateForm() {
         return false;
     }
 
-    // startDate 
+    // startDate : YYYY-MM-DD
+    start = startDate.split('-');
     if (startDate == "") {
         alert("startDate must be filled out!");
+        document.forms["form"]["Start_date"].focus();
+        return false;
+    }
+    else if(start[0] < 2022){
+        alert("Year is invalid, must be current year or upcoming year");
         document.forms["form"]["Start_date"].focus();
         return false;
     }
